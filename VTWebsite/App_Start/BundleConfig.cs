95ae1ad840088d4bd2cee7e5f40768533b6b4788
web developer kashif <webdeveloper.kashif@gmail.com>
2022-10-17 14:50:03 +0530

    Add project files.

---
+++ VTWebsite/App_Start/BundleConfig.cs
@@ -0,0 +1,44 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace VTWebsite
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/websitesJS").Include(
+            //    "~/Content/vendor/purecounter/purecounter_vanilla.js",
+            //    "~/Content/vendor/aos/aos.js",
+            //    "~/Content/vendor/bootstrap/js/bootstrap.bundle.min.js",
+            //    "~/Content/vendor/glightbox/js/glightbox.min.js",
+            //    "~/Content/vendor/isotope-layout/isotope.pkgd.min.js",
+            //    "~/Content/vendor/swiper/swiper-bundle.min.js"
+            //    ));
+
+                  bundles.Add(new StyleBundle("~/Content/css").Include(
+                     "~/Content/vendor/aos/aos.css",
+                     "~/Content/vendor/bootstrap/css/bootstrap.min.css",
+                     "~/Content/vendor/bootstrap-icons/bootstrap-icons.css",
+                     "~/Content/vendor/boxicons/css/boxicons.min.css",
+                     "~/Content/vendor/glightbox/css/glightbox.min.css",
+                     "~/Content/vendor/swiper/swiper-bundle.min.css",
+                     "~/Content/css/style.css"));
+        }
+    }
+}
---
+++ VTWebsite/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VTWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            return View();
+        }
+        public ActionResult Projects()
+        {
+
+            return View();
+        }
+        public ActionResult Galleries()
+        {
+
+            return View();
+        }
+
+        public ActionResult Blogs()
+        {
+
+            return View();
+        }
+        public ActionResult Partners()
+        {
+
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+           
+
+            return View();
+        }
+
+        public ActionResult ABC()
+        {
+            return View();
+        }
+    }
+}
